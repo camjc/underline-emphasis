6286d12639d15d96bf97070e2bc4a267b90b50fb
Cameron Chamberlain <git@camjc.com>
2019-09-29 18:23:49 +1000

    [patch] improving, seed by text

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,15 +1,26 @@
-import React, {Component} from 'react'
-import {render} from 'react-dom'
+import React, { Component } from "react";
+import { render } from "react-dom";
 
-import Example from '../../src'
+import Example from "../../src";
 
 class Demo extends Component {
   render() {
-    return <div>
-      <h1>underline-emphasis Demo</h1>
-      <Example>Foo Bar Baz</Example>
-    </div>
+    return (
+      <div>
+        <h1>
+          underline-emphasis Demo
+          <br />
+          <Example>Foo</Example>
+          <br />
+          <Example>Bar</Example>
+          <br />
+          <Example>Baz</Example>
+          <br />
+          <Example>Quux</Example>
+        </h1>
+      </div>
+    );
   }
 }
 
-render(<Demo/>, document.querySelector('#demo'))
+render(<Demo />, document.querySelector("#demo"));
--- src/index.js
+++ src/index.js
@@ -1,6 +1,48 @@
 import React from "react";
 
 /**
+ * @param {string} children
+ */
+const getSeed = children =>
+  children.split("").reduce((accumulator, _currentValue, index) => accumulator + children.charCodeAt(index), 0);
+
+const createLCGenerator = ({
+  seed = 1,
+  multiplier = 16807,
+  increment = 0,
+  modulus = 2147483647
+} = {}) => {
+  if (multiplier <= 0) {
+    throw Error("`multiplier` must be strictly larger than 0");
+  }
+  if (multiplier >= modulus) {
+    throw Error("`multiplier` must be smaller than `modulus`");
+  }
+  if (increment < 0) {
+    throw Error("`increment` must not be negative");
+  }
+  if (increment >= modulus) {
+    throw Error("`increment` must be smaller than `modulus`");
+  }
+  if (seed < 0) {
+    throw Error("`seed` must not be negative");
+  }
+  if (seed >= modulus) {
+    throw Error("`seed` must be smaller than `modulus`");
+  }
+  if (seed === 0 && increment === 0) {
+    throw Error("`seed` cannot be 0 if `increment` is 0");
+  }
+
+  var state = Math.abs(seed);
+  return () => {
+    var result = (state * multiplier + increment) % modulus;
+    state = result;
+    return result / modulus;
+  };
+};
+
+/**
  * @param {number} length
  */
 const makeEmptyArray = length =>
@@ -21,52 +63,76 @@ const isOdd = number => number % 2;
 const normalizeBetweenTwoRanges = (val, minVal, maxVal, newMin, newMax) =>
   newMin + ((val - minVal) * (newMax - newMin)) / (maxVal - minVal);
 
-/**
- * @param {number} length
- */
-const getRandomArray = length => makeEmptyArray(length).map(Math.random);
+const getArrayByFunction = ({ getArrayValue, length }) =>
+  makeEmptyArray(length).map(getArrayValue);
 
 /**
  * @param {number[]} array
  */
 const sortArrayNumerically = array => array.sort((a, b) => a - b);
 
-/**
- * @param {number[]} array
- */
-const normaliseArray = array =>
+const normaliseArray = ({ array = [], max = 1, min = 0 } = {}) =>
   array.map(value =>
     normalizeBetweenTwoRanges(
       value,
       Math.min(...array),
       Math.max(...array),
-      0,
-      1
+      min,
+      max
     )
   );
 
 const UnderlineEmphasis = ({ children }) => {
-  const maxWidth = 100;
-  const pointCount = 4 + Math.floor(Math.random() * 7);
+  const width = 100;
+  const height = 10;
+  const seed = getSeed(children);
+  const lcGenerator = createLCGenerator({ seed: seed });
 
-  const xArray = getRandomArray(pointCount);
-  const yArray = getRandomArray(pointCount);
+  const pointCountArray = getArrayByFunction({
+    getArrayValue: lcGenerator,
+    length: 3
+  });
+
+  const normalisedPointCountArray = normaliseArray({
+    array: pointCountArray,
+    max: 10
+  });
+
+  const pointCount = 5 + Math.floor(normalisedPointCountArray[0]);
+  console.log(pointCount);
+
+
+  const xArray = getArrayByFunction({
+    getArrayValue: lcGenerator,
+    length: pointCount
+  });
+  const yArray = getArrayByFunction({
+    getArrayValue: lcGenerator,
+    length: pointCount
+  });
 
   sortArrayNumerically(xArray);
   sortArrayNumerically(yArray).reverse();
 
-  const normalisedXArray = normaliseArray(xArray.map(value => value * 0.5));
-  const normalisedYArray = normaliseArray(yArray);
+  const normalisedXArray = normaliseArray({
+    array: xArray.map(
+      (value, index) => 0.5 + (isOdd(index) ? -1 : 1) * width * (value * 0.5)
+    ),
+    max: width
+  });
+  const normalisedYArray = normaliseArray({
+    array: yArray.map(value => value * 10),
+    max: height
+  });
 
-  const pointArray = normalisedXArray.map((value, index) => [
-    (0.5 + (isOdd(index) ? -1 : 1) * normalisedXArray[index]) * maxWidth,
-    normalisedYArray[index] * 10
+  const pointArray = normalisedXArray.map((_value, index) => [
+    normalisedXArray[index],
+    normalisedYArray[index]
   ]);
-  console.log("pointArray", pointArray);
 
   const pointString = pointArray.map(tuple => tuple.join(",")).join(" ");
 
-  const svgImage = `<svg width="100" height="10" viewBox="0 0 100 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+  const svgImage = `<svg width="${width}" height="10" viewBox="0 0 ${width} 10" fill="none" xmlns="http://www.w3.org/2000/svg">
   <polyline points="${pointString}" stroke="#9855D4" stroke-width="2" stroke-linecap="round" />
   </svg>`;
 
