354c4eebae3b73d0c988c8dded18a14776914478
Cameron Chamberlain <git@camjc.com>
2020-03-04 17:18:33 +1100

    [patch] stop clipping paths, round corners

--- src/underline-emphasis/index.tsx
+++ src/underline-emphasis/index.tsx
@@ -23,7 +23,7 @@ export const UnderlineEmphasis: React.FunctionComponent<{
     return null;
   }
   const width = 20 * children.length;
-  const height = 10;
+  const height = 16;
   const seed = getSeed(children);
   const lcGenerator = createLCGenerator({ seed: seed });
   const pointCountArray: number[] = getArrayByFunction({
@@ -46,17 +46,20 @@ export const UnderlineEmphasis: React.FunctionComponent<{
   });
   sortArrayNumerically(xArray);
   sortArrayNumerically(yArray).reverse();
+
+  const strokeWidth = 4;
+
   const normalisedXArray = normaliseArray({
     array: xArray.map(
       (value, index) => 0.5 + (isOdd(index) ? -1 : 1) * width * (value * 0.5)
     ),
-    max: width,
-    min: 0,
+    max: width - strokeWidth,
+    min: strokeWidth,
   });
   const normalisedYArray = normaliseArray({
     array: yArray.map(value => value * height),
-    max: height,
-    min: 0,
+    max: height - strokeWidth,
+    min: strokeWidth,
   });
   const pointArray = normalisedXArray.map((_value, index) => [
     normalisedXArray[index],
@@ -65,7 +68,7 @@ export const UnderlineEmphasis: React.FunctionComponent<{
   const pointString = pointArray.map(tuple => tuple.join(',')).join(' ');
 
   const svgImage = `<svg width="${width}" height="${height}" viewBox="0 0 ${width} ${height}" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <polyline points="${pointString}" stroke="#9855D4" stroke-width="5" stroke-linecap="round" />
+    <polyline points="${pointString}" stroke="#9855D4" stroke-width="${strokeWidth}" stroke-linecap="round" stroke-linejoin="round" />
   </svg>`;
 
   const encodedSvg = window.btoa(svgImage);
